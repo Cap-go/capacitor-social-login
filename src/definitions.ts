be12d22a0a0e602f7e5fb84aae30a606a6daa817
WcaleNieWolny <isupermichael007@gmail.com>
2025-01-23 06:30:29 +0100

    feat: disable one-tap (web)

--- src/definitions.ts
+++ src/definitions.ts
@@ -89,6 +89,12 @@ export interface GoogleLoginOptions {
    * @default false
    */
   forceRefreshToken?: boolean;
+  /**
+   * Disable one-tap login (web only)
+   * @description disable one-tap login
+   * @default false
+   */
+  disableOneTap?: boolean;
 }
 
 interface GoogleLoginResponseOnline {
