5372dd25ffa16b3256fa4265e13f4f0ba483233c
WcaleNieWolny <isupermichael007@gmail.com>
2025-10-21 17:48:38 +0200

    feat: update example-app android

--- example-app/capacitor.config.ts
+++ example-app/capacitor.config.ts
@@ -4,6 +4,9 @@ const config: CapacitorConfig = {
   appId: 'app.capgo.plugin.SocialLogin',
   appName: 'a',
   webDir: 'dist',
+  server: {
+    url: 'https://example-webapp-08.beta.localcan.dev/'
+  }
 };
 
 export default config;
--- example-app/vite.config.ts
+++ example-app/vite.config.ts
@@ -4,4 +4,7 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['localhost', '192.168.1.100', 'example-webapp-08.beta.localcan.dev'],
+  }
 });
